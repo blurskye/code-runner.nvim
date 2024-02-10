5b8f2cd1ba78def49e8487a166dfc6ed1a33fb1c
desync <blurskye@proton.me>
2024-02-10 20:28:19 +0600

    testing

--- lua/code-runner.lua
+++ lua/code-runner.lua
@@ -1,42 +1,9 @@
--- local M = {}
-
--- function M.setup(opts)
---   M.opts = opts or {}
---   M.opts.commands_path = M.opts.commands_path or vim.fn.stdpath('config') .. '/lua/code-runner/commands.json'
---   M.opts.keymap = M.opts.keymap or '<F5>'
-
---   -- Set the keymap
---   vim.api.nvim_set_keymap('n', M.opts.keymap, ':lua require("code-runner").run_code()<CR>',
---     { noremap = true, silent = true })
--- end
-
--- function M.run_code()
---   print("stating")
---   local file_path = vim.fn.expand("%:p")
---   local file_extension = vim.fn.fnamemodify(file_path, ":e")
-
---   local cmd
-
---   if file_extension == 'py' then
---     cmd = 'python -u %s'
---   end
-
---   if cmd then
---     cmd = string.format(cmd, file_path)
---     print("Running command: " .. cmd)
---     vim.cmd("execute 'TermExec cmd=\"" .. cmd .. "\"'", false)
---   else
---     print("Error: Could not construct command for file extension " .. file_extension)
---   end
--- end
-
--- return M
 local M = {}
 
 -- Default commands
 M.commands = {
     py = 'python -u %s',
-    -- Add more default commands here
+    -- todo more commands
 }
 
 function M.setup(opts)
@@ -50,32 +17,45 @@ function M.setup(opts)
         end
     end
 
+    if M.opts.run_tmux ~= false then
+        vim.cmd("TermExec cmd='tmux new-session -A -s nvim'")
+    end
+
     -- Set the keymap
     vim.api.nvim_set_keymap('n', M.opts.keymap, ':lua require("code-runner").run_code()<CR>',
         { noremap = true, silent = true })
 end
 
+-- function M.run_code()
+--     print("Starting")
+--     local file_path = vim.fn.expand("%:p")
+--     local file_extension = vim.fn.fnamemodify(file_path, ":e")
+
+--     local cmd = M.commands[file_extension]
+
+--     if cmd then
+--         cmd = string.format(cmd, file_path)
+--         print("Running command: " .. cmd)
+--         vim.cmd("execute 'TermExec cmd=\"" .. cmd .. "\"'")
+--     else
+--         print("Error: Could not construct command for file extension " .. file_extension)
+--     end
+-- end
+
 function M.run_code()
-    -- if not _G.tmux_started then
-    --   print("Starting tmux")
-    --   vim.cmd(
-    --     [[execute 'TermExec cmd="[ -z "$TMUX" ] && (tmux has-session -t nvim 2>/dev/null || tmux new-session -d -s nvim) && tmux attach -t nvim"']])
-    --   _G.tmux_started = true
-    -- end
-    if not _G.tmux_started then
-        print("Starting tmux")
-        vim.cmd(
-            [[execute 'TermExec cmd="bash -c \\"[ -z \\\\\\"$TMUX\\\\\\" ] && (tmux has-session -t nvim 2>/dev/null || tmux new-session -d -s nvim) && tmux attach -t nvim\\""']])
-        _G.tmux_started = true
-    end
     print("Starting")
     local file_path = vim.fn.expand("%:p")
+    local file_dir = vim.fn.expand("%:p:h")
+    local file_name = vim.fn.expand("%:t")
+    local file_name_without_ext = vim.fn.expand("%:r:t")
     local file_extension = vim.fn.fnamemodify(file_path, ":e")
 
     local cmd = M.commands[file_extension]
 
     if cmd then
-        cmd = string.format(cmd, file_path)
+        cmd = cmd:gsub("$dir", file_dir)
+        cmd = cmd:gsub("$fileName", file_name)
+        cmd = cmd:gsub("$fileNameWithoutExt", file_name_without_ext)
         print("Running command: " .. cmd)
         vim.cmd("execute 'TermExec cmd=\"" .. cmd .. "\"'")
     else
