db38ca9a25b7558f1ed47b2d1966c692736f89b1
desync <blurskye@proton.me>
2024-02-13 21:59:06 +0600

    updated readme with finalised experimental feature in the last commit

--- lua/code-runner.lua
+++ lua/code-runner.lua
@@ -1,11 +1,181 @@
 local M = {}
+local function table_to_string(data, indent)
+    if not data then
+        return ""
+    end
+
+    local str = ""
+    for k, v in pairs(data) do
+        if type(v) == "table" then
+            str = str .. string.rep(" ", indent) .. "- " .. table_to_string(v, indent + 2) .. "\n"
+        else
+            str = str .. string.rep(" ", indent) .. tostring(v) .. "\n"
+        end
+    end
+
+    return str
+end
+
+-- function M.unbind_commands(json_data)
+--     for _, data in pairs(json_data) do
+--         if vim.api.nvim_get_keymap('n')[data.key] then
+--             vim.api.nvim_del_keymap('n', data.key)
+--         end
+--     end
+-- end
+
+function M.unbind_commands(json_data)
+    if (json_data) then
+        for _, v in pairs(json_data) do
+            if v.command and v.keybind then
+                -- Unbind the keymap in normal mode
+                vim.api.nvim_set_keymap('n', v.keybind, '', { noremap = true, silent = true })
+            end
+        end
+    end
+end
+
+-- function M.bind_commands(json_data)
+--     if (json_data) then
+--         for _, v in pairs(json_data) do
+--             if v.command and v.keybind then
+--                 local file_buffer = vim.api.nvim_buf_get_name(0)
+--                 local file_path = '"' .. file_buffer .. '"'
+
+--                 local file_dir = vim.fn.fnamemodify(file_path, ":h")
+--                 local file_name = vim.fn.fnamemodify(file_path, ":t")
+--                 local file_name_without_ext = vim.fn.fnamemodify(file_path, ":r:t")
+--                 local file_extension = vim.fn.fnamemodify(file_path, ":e")
+
+--                 local cmd = v.command
+--                 cmd = cmd:gsub("$dir", file_dir)
+--                 cmd = cmd:gsub("$fileNameWithoutExt", file_name_without_ext)
+--                 cmd = cmd:gsub("$fileName", file_name)
+--                 cmd = cmd:gsub("$fileExtension", file_extension)
+--                 cmd = cmd:gsub("$filePath", file_path)
+
+
+
+--                 vim.api.nvim_set_keymap('n', v.keybind,
+--                     ":TermExec cmd='" .. cmd .. "'<CR>",
+--                     { noremap = true, silent = true })
+--             end
+--         end
+--     end
+-- end
+-- function M.bind_commands(json_data)
+--     if (json_data) then
+--         for _, v in pairs(json_data) do
+--             if v.command and v.keybind then
+--                 local file_buffer = vim.api.nvim_buf_get_name(0)
+--                 local file_path = file_buffer
+
+--                 local file_dir = vim.fn.fnamemodify(file_path, ":h")
+--                 local file_name = vim.fn.fnamemodify(file_path, ":t")
+--                 local file_name_without_ext = vim.fn.fnamemodify(file_path, ":r:t")
+--                 local file_extension = vim.fn.fnamemodify(file_path, ":e")
+
+--                 local cmd = v.command
+--                 cmd = cmd:gsub("$dir", file_dir)
+--                 cmd = cmd:gsub("$fileNameWithoutExt", file_name_without_ext)
+--                 cmd = cmd:gsub("$fileName", file_name)
+--                 cmd = cmd:gsub("$fileExtension", file_extension)
+--                 cmd = cmd:gsub("$filePath", file_path)
+
+--                 vim.api.nvim_set_keymap('n', v.keybind,
+--                     ":TermExec cmd='" .. cmd .. "'<CR>",
+--                     { noremap = true, silent = true })
+--             end
+--         end
+--     end
+-- end
+
+
+-- function M.bind_commands(json_data)
+--     if (json_data) then
+--         for _, v in pairs(json_data) do
+--             if v.command and v.keybind then
+--                 local file_buffer = vim.api.nvim_buf_get_name(0)
+
+--                 local file_path = '"' .. file_buffer .. '"'
+--                 local file_dir = '"' .. vim.fn.fnamemodify(file_buffer, ":h") .. '"'
+--                 local file_name = '"' .. vim.fn.fnamemodify(file_buffer, ":t") .. '"'
+--                 local file_name_without_ext = '"' .. vim.fn.fnamemodify(file_buffer, ":t:r") .. '"'
+--                 local file_extension = '"' .. vim.fn.fnamemodify(file_buffer, ":e") .. '"'
+
+--                 local cmd = v.command
+--                 cmd = cmd:gsub("$dir", file_dir)
+--                 cmd = cmd:gsub("$fileNameWithoutExt", file_name_without_ext)
+--                 cmd = cmd:gsub("$fileName", file_name)
+--                 cmd = cmd:gsub("$fileExtension", file_extension)
+--                 cmd = cmd:gsub("$filePath", file_path)
+
+--                 vim.api.nvim_set_keymap('n', v.keybind,
+--                     ":TermExec cmd='" .. cmd .. "'<CR>",
+--                     { noremap = true, silent = true })
+--             end
+--         end
+--     end
+-- end
+function M.adjust_command_path()
+    local data = M.coderun_json -- Accessing cached loaded data
+
+    if data then
+        return data.dir or vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h") -- Use data.dir or current dir
+    end
+
+    return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h") -- Default to current dir
+end
+
+function M.bind_commands(json_data)
+    if (json_data) then
+        for _, v in pairs(json_data) do
+            if v.command and v.keybind then
+                local file_buffer = vim.api.nvim_buf_get_name(0)
+
+                -- local file_path = '"' .. file_buffer .. '"'
+                local file_path = '"' .. M.adjust_command_path() .. '"'
+                local file_dir = '"' .. vim.fn.fnamemodify(file_buffer, ":h") .. '"'
+                local file_name = '"' .. vim.fn.fnamemodify(file_buffer, ":t") .. '"'
+                local file_name_without_ext = '"' .. vim.fn.fnamemodify(file_buffer, ":t:r") .. '"'
+                local file_extension = '"' .. vim.fn.fnamemodify(file_buffer, ":e") .. '"'
+
+                local cmd = v.command
+                cmd = cmd:gsub("$dir", file_dir)
+                cmd = cmd:gsub("$fileNameWithoutExt", file_name_without_ext)
+                cmd = cmd:gsub("$fileName", file_name)
+                cmd = cmd:gsub("$fileExtension", file_extension)
+                cmd = cmd:gsub("$filePath", file_path)
+
+
+
+                -- Check if the keybind already exists
+                local exists = vim.api.nvim_exist_autocmd({
+                    group = vim.api.nvim_get_runtime_path(),
+                    cmd = "nnoremap " .. v.keybind,
+                })
+
+                if not exists then
+                    -- Keybind doesn't exist, safe to bind it
+                    vim.api.nvim_set_keymap('n', v.keybind,
+                        ":TermExec cmd='" .. cmd .. "'<CR>",
+                        { noremap = true, silent = true })
+                else
+                    -- Keybind already exists, handle appropriately
+                    -- print("Keybind " .. v.keybind .. " already exists! Skipping binding.")
+                end
+            end
+        end
+    end
+end
 
 function M.load_json()
     local bufnr = vim.api.nvim_win_get_buf(0)
     local file_path = vim.api.nvim_buf_get_name(bufnr)
     local file_dir = vim.fn.fnamemodify(file_path, ":h")
+    local root_dir = "/"
 
-    while file_dir ~= "/" do
+    while file_dir ~= root_dir do
         local json_path = file_dir .. "/coderun.json"
         local file = io.open(json_path, "r")
 
@@ -13,19 +183,20 @@ function M.load_json()
             local content = file:read("*all")
             file:close()
 
-            local data = json.decode(content)
+            local data = vim.fn.json_decode(content)
 
-            for _, v in pairs(data) do
-                if v.command and v.keybind then
-                    return v.command, v.keybind
-                end
-            end
+            -- Return the whole JSON data as a Lua table
+            return data
         end
 
-        file_dir = vim.fn.fnamemodify(file_dir, ":h")
+        local parent_dir = vim.fn.fnamemodify(file_dir, ":h")
+        if parent_dir == file_dir then -- We've reached the root directory
+            break
+        end
+        file_dir = parent_dir
     end
 
-    return nil, nil
+    return nil
 end
 
 M.commands = {
@@ -34,7 +205,7 @@ M.commands = {
     typescript = "deno run $dir/$fileName",
     rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
     c = "cd $dir && gcc $fileName -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
-    cpp = "cd $dir && g++ $fileName -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
+    cpp = "cd $dir && g++ $fileName -o $dir/$fileNameWithoutExt && $dir/$fileNameWithoutExt",
     javascript = "node $dir/$fileName",
     php = "php $dir/$fileName",
     ruby = "ruby $dir/$fileName",
@@ -69,6 +240,20 @@ M.extensions = {
     pascal = { "pas" },
     nim = { "nim" }
 }
+function M.generate_commands_table(file_extension)
+    local commands_table = {}
+    for language, extensions in pairs(M.extensions) do
+        for _, extension in ipairs(extensions) do
+            if extension == file_extension then
+                commands_table["run " .. language .. " project"] = {
+                    command = M.commands[language],
+                    keybind = M.opts.keymap
+                }
+            end
+        end
+    end
+    return commands_table
+end
 
 function M.setup(opts)
     M.opts = opts or {}
@@ -86,64 +271,60 @@ function M.setup(opts)
         end
     end
 
-    -- if M.opts.run_tmux ~= false then
-    --   vim.cmd("TermExec cmd='tmux new-session -A -s nvim'")
-    --   vim.cmd("ToggleTerm")
-    -- end
-    if M.opts.run_tmux == true then
+    if M.opts.run_tmux ~= false then
         vim.cmd("TermExec cmd='tmux new-session -A -s nvim'")
         vim.cmd("ToggleTerm")
     end
-
-    -- Set the keymap
-    vim.api.nvim_set_keymap('n', M.opts.keymap, ':lua require("code-runner").run_code()<CR>',
-        { noremap = true, silent = true })
+    M.coderun_json = M.load_json()
+    -- if (M.json_data) then
+    --     M.bind_commands(M.json_data)
+    -- else
+    --     local file_extension = vim.fn.expand("%:e")
+    --     M.bind_commands(M.generate_commands_table(file_extension))
+    -- end
+    if (M.coderun_json) then
+        M.bind_commands(M.coderun_json)
+    else
+        M.coderun_json = M.generate_commands_table(vim.fn.expand("%:e"))
+        M.bind_commands(M.coderun_json)
+    end
+    -- print(M.table_to_string(M.json_data))
+    vim.api.nvim_exec([[
+            augroup CodeRunner
+                autocmd!
+                autocmd BufEnter * lua require('code-runner').handle_buffer_enter()
+                autocmd BufLeave * lua require('code-runner').handle_buffer_exit()
+                augroup END
+                ]], false)
 end
 
-function M.run_code()
-    print("Starting")
-    -- Get the current window's buffer number
-    local bufnr = vim.api.nvim_win_get_buf(0)
-    -- Check if the current window contains a terminal, if so then run the code in the window above
-    if vim.api.nvim_buf_get_option(bufnr, "buftype") == "terminal" then
-        local wins = vim.api.nvim_tabpage_list_wins(0)
-        for i, win in ipairs(wins) do
-            if win == vim.api.nvim_get_current_win() and i > 1 then
-                bufnr = vim.api.nvim_win_get_buf(wins[i - 1])
-                break
-            end
+function M.handle_buffer_enter()
+    local buftype = vim.api.nvim_buf_get_option(0, 'buftype')
+    if buftype ~= 'terminal' and buftype ~= 'nofile' and buftype == '' then
+        M.coderun_json = M.load_json()
+        M.json_data = M.load_json()
+        -- print(M.table_to_string(M.json_data))
+
+        if (M.coderun_json) then
+            M.bind_commands(M.coderun_json)
+        else
+            M.coderun_json = M.generate_commands_table(vim.fn.expand("%:e"))
+            M.bind_commands(M.coderun_json)
         end
     end
-    local file_path = vim.api.nvim_buf_get_name(bufnr)
-    local file_dir = vim.fn.fnamemodify(file_path, ":h")
-    local file_name = vim.fn.fnamemodify(file_path, ":t")
-    local file_name_without_ext = vim.fn.fnamemodify(file_path, ":r:t")
-    local file_extension = vim.fn.fnamemodify(file_path, ":e")
+end
 
-    local language
-    for lang, exts in pairs(M.extensions) do
-        for _, ext in ipairs(exts) do
-            if ext == file_extension then
-                language = lang
-                break
-            end
+function M.handle_buffer_exit()
+    local buftype = vim.api.nvim_buf_get_option(0, 'buftype')
+    -- print("buff type if (" .. buftype .. ")")
+    if buftype == 'nofile' then
+        -- print("tried to unbind")
+        if M.coderun_json then
+            M.unbind_commands(M.coderun_json)
+        else
+            local file_extension = vim.fn.expand("%:e")
+            M.bind_commands(M.generate_commands_table(file_extension))
         end
-        if language then break end
-    end
-
-    local cmd = M.commands[language]
-
-    if cmd then
-        cmd = cmd:gsub("$dir", file_dir)
-        cmd = cmd:gsub("$fileNameWithoutExt", file_name_without_ext)
-        cmd = cmd:gsub("$fileName", file_name)
-        cmd = cmd:gsub("$fileExtension", file_extension)
-        cmd = cmd:gsub("$language", language)
-        cmd = cmd:gsub("$filePath", file_path)
-        print("Running command: " .. cmd)
-        vim.cmd("execute 'TermExec cmd=\"" .. cmd .. "\"'")
-    else
-        print("Error: Could not construct command for language " .. (language or file_extension))
     end
 end
 
--- readme.md
+++ readme.md
@@ -1,11 +1,12 @@
 <head>
-<div>
+<div align="center">
 <img src="https://raw.githubusercontent.com/blurskye/code-runner.nvim/main/banner.png">
 
 <!-- ### Stand Up For <span style="color:green"> Humanity </span>, Oppose <span style="color:red">Genocide</span> and <span style="color:red">Suppression</span>
 ### Condemn <span style="color:red">75 years </span> of <span style="color:red"> Brutal Occupation </span> and <span style="color:red"> Genocide </span>
 ### <span style="color:green">Support PALESTINE<img src="https://raw.githubusercontent.com/blurskye/code-runner.nvim/main/icon.png" alt="heart" style="vertical-align: middle; position: relative; top: -2px;"></span>
 </div> -->
+</div align="center">
 </head>
 <div align="center">
 
