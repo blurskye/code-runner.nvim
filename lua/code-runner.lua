0925c763b7fee52aa9c33d2b334060db1df8c7ba
desync <blurskye@proton.me>
2024-03-01 10:52:14 +0600

    debug commit

--- lua/code-runner.lua
+++ lua/code-runner.lua
@@ -479,7 +479,8 @@ function M.preview_file(file_path)
 end
 
 function M.load_json()
-    local bufnr = vim.api.nvim_win_get_buf(0)
+    local buf_id = vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win())
+    local bufnr = vim.api.nvim_win_get_buf(buf_id)
     local file_path = vim.api.nvim_buf_get_name(bufnr)
     local file_dir = vim.fn.fnamemodify(file_path, ":h")
     local root_dir = "/"
