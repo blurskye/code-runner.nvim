4294aa374778958c6a97e963da715e0852ea440c
desync <blurskye@proton.me>
2024-03-03 10:59:08 +0600

    fixed bugs

--- lua/code-runner.lua
+++ lua/code-runner.lua
@@ -345,7 +345,6 @@ function M.handle_buffer_exit()
     local win_id = vim.api.nvim_get_current_win()
     local buf_id = vim.api.nvim_win_get_buf(win_id)
     local buftype = vim.api.nvim_buf_get_option(buf_id, 'buftype')
-    print("unbinding buffer type:" .. buftype)
     -- if buftype == 'nofile' or buftype == "" then
     if buftype == 'nofile' then
         if M.coderun_json then
